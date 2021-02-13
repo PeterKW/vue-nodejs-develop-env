0c8347a04baf6d15e8ae7e67b36a353be676b1fc
Peter <pkwjn1633@gmail.com>
2021-02-13 14:48:42 +0000

    Got working locally

--- my-app/vue.config.js
+++ my-app/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '^/api': {
-          target: 'http://localhost:3080',
+          target: 'http://node-api:3080',
           changeOrigin: true
         },
       }
